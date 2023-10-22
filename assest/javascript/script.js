175ad91eedf61fb31eea443bcea53679b813c547
Sang <internship.sang@gmail.com>
2023-10-22 11:24:17 +0700

    update submit form

--- assest/javascript/script.js
+++ assest/javascript/script.js
@@ -1,41 +1,23 @@
 function handleSubmitForm(event) {
-  // event.preventDefault();
+  event.preventDefault();
 
   const fullName = document.getElementById("fullName").value;
   const job = document.getElementById("job").value;
   const phoneNumber = document.getElementById("phoneNumber").value;
   const address = document.getElementById("address").value;
 
-  // console.log("log fullName: ", fullName.value);
-  // console.log("log job: ", job.value);
-  // console.log("log phoneNumber: ", phoneNumber.value);
-  // console.log("log address: ", address.value);
-
   console.log("log fullName: ", fullName);
   console.log("log job: ", job);
   console.log("log phoneNumber: ", phoneNumber);
   console.log("log address: ", address);
 
-  const recipient = "sangpm3@gmail.com";
-  const subject = "Thông tin submit";
-  const body = `Họ và tên: ${fullName}. Nghề nghiệp: ${job}. Số điện thoại: ${phoneNumber}. Địa chỉ: ${address}`;
-
-  const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
-    subject
-  )}&body=${encodeURIComponent(body)}`;
-
-  // window.location.href = mailtoLink;
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "2dulieucuatoi@gmail.com",
-    Password: "E45A5330E42FBFFDC84673296498AE2D5883",
-    From: "2dulieucuatoi@gmail.com",
-
-    To: "sangpm3@gmail.com",
-    Subject: "Sending Email using javascript",
-    Body: body,
-  }).then(function (message) {
-    alert("mail sent successfully");
+  jQuery.ajax({
+    url: `https://docs.google.com/forms/d/e/1FAIpQLSdUhVH-nUEJk5tYEG2bbKW9FjE2zYLsmd_ImHF71p0ZEtrDxg/formResponse?submit=Submit?usp=pp_url&entry.1343648701=${fullName}&entry.1853088013=${job}&entry.2000615805=${phoneNumber}&entry.1047505472=${address}`,
+    type: "post",
+    // data: { name: "yogesh", salary: 35000, email: "yogesh@makitweb.com" },
+    success: function (response) {
+      console.log("log response: ", response);
+    },
   });
 }
 
