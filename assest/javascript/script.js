2ea6d3785120be049c36468e084dc9ad8ff0c08c
Sang <internship.sang@gmail.com>
2023-10-29 11:28:41 +0700

    update code

--- assest/javascript/script.js
+++ assest/javascript/script.js
@@ -1,4 +1,9 @@
-function handleSubmitForm(event) {
+const FULL_NAME_ID = 1343648701;
+const JOB_ID = 1853088013;
+const PHONE_NUMBER_ID = 2000615805;
+const ADDRESS_ID = 1047505472;
+
+async function handleSubmitForm(event) {
   event.preventDefault();
 
   const fullName = document.getElementById("fullName").value;
@@ -11,14 +16,15 @@ function handleSubmitForm(event) {
   console.log("log phoneNumber: ", phoneNumber);
   console.log("log address: ", address);
 
-  jQuery.ajax({
-    url: `https://docs.google.com/forms/d/e/1FAIpQLSdUhVH-nUEJk5tYEG2bbKW9FjE2zYLsmd_ImHF71p0ZEtrDxg/formResponse?submit=Submit?usp=pp_url&entry.1343648701=${fullName}&entry.1853088013=${job}&entry.2000615805=${phoneNumber}&entry.1047505472=${address}`,
+  await jQuery.ajax({
+    url: `https://docs.google.com/forms/d/e/1FAIpQLSdUhVH-nUEJk5tYEG2bbKW9FjE2zYLsmd_ImHF71p0ZEtrDxg/formResponse?submit=Submit?usp=pp_url&entry.${FULL_NAME_ID}=${fullName}&entry.${JOB_ID}=${job}&entry.${PHONE_NUMBER_ID}=${phoneNumber}&entry.${ADDRESS_ID}=${address}`,
     type: "post",
-    // data: { name: "yogesh", salary: 35000, email: "yogesh@makitweb.com" },
     success: function (response) {
       console.log("log response: ", response);
     },
   });
+
+  alert("Submit success");
 }
 
 const form = document.getElementById("submitForm");
